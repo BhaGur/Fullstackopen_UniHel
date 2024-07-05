3a4dd50e049884f09c1672f3472eaa894cede63d
Bhabishya Gurung <bhab_ej@hotmail.com>
2024-07-05 15:04:40 +0300

    2.17 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,14 @@ import Search from './components/Search'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
+  const [notification, setNotification] = useState(null);
 
   const hook = () => {
     personService
@@ -38,7 +40,8 @@ const App = () => {
       number: newNumber
     }
 
-    const isNameExist = persons.find(person => person.name === personObject.name);
+    const isNameExist = persons.find(person => person.name.toLowerCase() === personObject.name.toLocaleLowerCase());
+    const updateContact = {...isNameExist, number: newNumber}
 
     if (isNameExist) {
       if (isNameExist.number != personObject.number) {
@@ -48,25 +51,40 @@ const App = () => {
           )
         )
          personService
-          .update(isNameExist.id, personObject)
-          .then((updatedPerson) => 
+          .update(isNameExist.id, updateContact)
+          .then((updatedPerson) => {
             setPersons(
               persons.map((person) =>
                 person.id === updatedPerson.id ? updatedPerson : person
               )
             )
-          );
-    } 
-      setNewName('');
-      setNewNumber('');
-      return;
-    }  
+            setNewName('')
+            setNewName('')
+            setNotification({status: 'success', text: `${personObject.name} update with new number`});
+            setTimeout(() => setNotification(null), 5000);
+          })
+          .catch((error) => {
+            console.log(error);
+            setNotification({ status: 'error', text: `This ${personObject.name} has already been removed from the server`})
+          })
+      } 
+    
+    } else {
       personService
       .create(personObject)
-      .then((returnedPerson) => 
-        setPersons(persons.concat(returnedPerson)));
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson));
         setNewName('');
-        setNewNumber('');   
+        setNewNumber('');
+        setNotification({status: 'success', text:`${personObject.name} added`});
+        setTimeout(() => setNotification(null), 5000);
+      })
+      .catch((error) => {
+        console.log(error)
+        setNotification({status: 'error', text: `Adding failed`})
+        setTimeout(() => setNotification(null), 5000);
+      })     
+    }   
   };
 
   const handleDelete = (id) => {
@@ -89,7 +107,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <Notification message = {notification} />
       <Search value={searchTerm} onChange={handleSearch} />
 
       <h2>add a new</h2>
