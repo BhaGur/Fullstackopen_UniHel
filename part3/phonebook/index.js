9ab4185d60f0c11d9560177f9771485f7fad590a
Bhabishya Gurung <bhab_ej@hotmail.com>
2024-09-16 13:30:25 +0300

    Part 3.9 completed installing cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.format('tiny', (tokens, req, res) => {
     return [
@@ -106,7 +109,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
